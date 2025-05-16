18ee231f8f398c5a615dfb2b34ec9409120de2bd
ddewantaraq <muhammad.dimas.dewantara@gmail.com>
2025-05-16 11:18:01 +0700

    update

--- src/mastra/memory/index.ts
+++ src/mastra/memory/index.ts
@@ -20,7 +20,7 @@ export const storage = new PostgresStore({
 // Initialize memory with PostgreSQL storage and vector search
 export const ninjaChefMemory = new Memory({
   storage: storage,
-  vector: new PgVector({ connectionString }),
+  vector: new PgVector({ connectionString, pgPoolOptions: {ssl: false} }), // not recommended for production
   embedder: openai.embedding("text-embedding-3-small"),
   options: {
     threads: {
